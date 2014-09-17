8743965f0ce2623e652d39dd39421fce673a4f35
Paulo Cesar <email@pocesar.e4ward.com>
2014-09-17 05:36:19 -0300

    done

--- README.md
+++ README.md
@@ -10,24 +10,23 @@ Doesn't allow user logins, only public keys.
 ## Defaults
 
 ```
-USER = git
-PUBLIC_KEY = ""
-IN = ""
-OUT = ""
+USER = git # The user used in the git push
+PUBLIC_KEY = "" # Your mounted public key path inside the container
+IN = "" # The folder that holds the git bare repo
+OUT = "" # The folder that receives the git checkout
 ```
 
 ## Setup
 
 ```bash
-$ docker run -p 1234:22 -d --name deploy -e OUT="/repo" -e PUBLIC_KEY="/id_dsa.pub" -v ~/.ssh/id_dsa.pub:/id_dsa.pub -v  /var/www:/repo pocesar/docker-git-deploy
+$ docker run -d -p 1234:22 \
+    --name deploy \
+    -e PUBLIC_KEY="/id_dsa.pub" \
+    -v ~/.ssh/id_dsa.pub:/id_dsa.pub \
+    -e OUT="/repo" \
+    -v  /var/www:/repo \
+    pocesar/docker-git-deploy
 c48f7b86594953012ca4731b1ec08b053ce5826d3f501ed579c660bec42d2c88
-
-$ docker logs deploy
-[+] Created user git
-Server listening on 0.0.0.0 port 22.
-Server listening on :: port 22.
-Accepted publickey for docker from 172.17.42.1 port 53228 ssh2: DSA 15:44:0c:ab:a3:b4:49:30:1c:24:3a:76:28:11:1b:e8
-Received disconnect from 172.17.42.1: 11: disconnected by user
 ```
 
 ## Deploy
@@ -37,3 +36,22 @@ git remote add upstream ssh://docker@yourhost:1234/~/repo.git # or /home/docker/
 git commit -m "Behold!"
 git push upstream
 ```
+
+## Get logs (with colors!)
+
+```
+$ docker logs deploy
+[+] 2014-09-17T08:35:03Z: Created user docker
+[+] 2014-09-17T08:35:03Z: Using existing path
+Initialized empty shared Git repository in /in/
+[+] 2014-09-17T08:35:03Z: Will deploy to /out. Deploy using this git remote url: ssh://docker@host:port/in
+Server listening on 0.0.0.0 port 22.
+Server listening on :: port 22.
+Accepted publickey for docker from 172.17.42.1 port 45608 ssh2: RSA 79:4f:46:33:1f:39:25:6d:0d:37:e1:e0:d2:42:5c:0e
+[^] 2014-09-17T08:35:03Z: Updated sources on /out
+-------------
+51da9c1 - Paulo Cesar, 24 hours ago: need to fix permissions
+-------------
+Received disconnect from 172.17.42.1: 11: disconnected by user
+```
+
--- run.sh
+++ run.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+function initGit {
+    mkdir -p -m 660 $GIT_DIR
+    chown -R $USER:root $GIT_DIR
+    git init --bare --shared=0660
+}
+
+FORMAT='%Y-%m-%dT%H:%M:%SZ'
+
+function log {
+    echo -e "\e[93m[+] $(date -u +$FORMAT): \e[32m$1\e[0m"
+}
+
 if [[ ! -e "$PUBLIC_KEY" ]]; then
-    echo "[-] Missing public key"
+    log "Missing public key"
     exit 1
 elif [[ ! -e "$OUT" ]]; then
-    echo "[-] Need to set OUT environment variable"
-    exit 1
+    log "Need to set OUT environment variable"
+    exit 2
 else
     export HOME=/home/$USER
+    export MEM_LOG=/dev/shm/$USER
+
+    touch $MEM_LOG
+    chmod 0777 $MEM_LOG
+
     useradd -s /bin/bash -m -d $HOME -g root $USER
     mkdir -p -m 700 $HOME/.ssh
     cat $PUBLIC_KEY > $HOME/.ssh/authorized_keys
@@ -15,27 +32,41 @@ else
     sed -ri "s@#?AuthorizedKeysFile\s+.*@AuthorizedKeysFile $HOME/.ssh/authorized_keys@" /etc/ssh/sshd_config
     chown -R $USER:root $HOME/.ssh
 
-    echo "[+] Created user $USER"
+    log "Created user $USER"
 
-    if [[ ! -e "$IN" ]]; then
-        echo "[+] Creating empty repository"
+    if [ ${IN} ]; then
+        log "Using existing path"
+        export GIT_DIR=$IN
+    else
+        log "Creating empty repository"
         export GIT_DIR=$HOME/repo.git
-        mkdir -p $GIT_DIR
-        chown -R $USER $GIT_DIR
-        git init --bare --shared=0660
-        touch $GIT_DIR/hooks/post-receive
-        chmod +x $GIT_DIR/hooks/post-receive
+    fi
+
+    initGit
+
+    if [[ $(cd $GIT_DIR && git rev-parse --is-inside-work-tree) ]]; then
+        if [[ $(cd $GIT_DIR && git rev-parse --is-bare-repository) ]]; then
+            touch $GIT_DIR/hooks/post-receive
+            chmod +x $GIT_DIR/hooks/post-receive
 (
 cat <<POSTRECEIVE
 #!/bin/bash
 
 GIT_WORK_TREE="$OUT" git checkout -f master
+echo -e "\e[93m[^] $(date -u +$FORMAT): \e[32mUpdated sources on $OUT\e[0m" >> $MEM_LOG
+git log -1 --decorate --pretty=format:"%h - %an, %ar: %s" | xargs -I {} echo -e "-------------\n\e[32m{}\e[0m\n-------------" >> $MEM_LOG
 POSTRECEIVE
 ) > $GIT_DIR/hooks/post-receive
+        else
+            log "Invalid git bare repo"
+            exit 3
+        fi
     else
-        echo "[+] Using existing repository"
-        export GIT_DIR=$IN
+        echo "[+] $NOW: Trying to re-init bare git repo"
+        initGit
     fi
 
-    $(which sshd) -D -E /dev/stderr
+    log "Will deploy to $OUT. Deploy using this git remote url: ssh://$USER@host:port$GIT_DIR"
+
+    tail -f $MEM_LOG & $(which sshd) -D -E $MEM_LOG
 fi
